42a35fb870572cc084b19577ef1f7120fc5f3a27
Molly <30417297+mmutton@users.noreply.github.com>
2017-12-22 15:50:40 -0800

    Upload StreamOn

---
+++ StreamOn/StreamOn/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StreamOn.Models;
+
+namespace StreamOn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "StreamOn is unaffiliated with any service or compnay and exists just for fun!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Music Streaming Service Guide";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
